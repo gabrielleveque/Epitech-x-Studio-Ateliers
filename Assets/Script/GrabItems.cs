68cb30f97ee48d3b532f0ff41c5121688879bb35
Florianperidy <florian.peridy@epitech.eu>
2025-01-14 15:31:24 +0100

    [+]Pushing coins (Mulhouse gears) as prefabs, and three gears at the top right of the screen to display how much coins the player has.

--- Assets/Script/GrabItems.cs
+++ Assets/Script/GrabItems.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class ItemGrab : MonoBehaviour
 {
+    public GameObject Roue_Mulhouse1, Roue_Mulhouse2, Roue_Mulhouse3;
     private bool IsPickeable;
+    public int NumberGears;
+    public GameObject Roue;
 
     private void Start()
     {
         IsPickeable = false;
+        NumberGears = 0;
+        // Roue_Mulhouse1.gameObject.SetActive(false);
+        // Roue_Mulhouse2.gameObject.SetActive(false);
+        // Roue_Mulhouse3.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -17,22 +24,59 @@ public class ItemGrab : MonoBehaviour
         {
             PickItem();
         }
+        if (NumberGears > 3)
+            NumberGears = 3;
+        if (NumberGears == 3)
+        {
+            Roue_Mulhouse1.gameObject.SetActive(true);
+            Roue_Mulhouse2.gameObject.SetActive(true);
+            Roue_Mulhouse3.gameObject.SetActive(true);
+            return;
+        }
+        if (NumberGears == 2)
+        {
+            Roue_Mulhouse1.gameObject.SetActive(true);
+            Roue_Mulhouse2.gameObject.SetActive(true);
+            Roue_Mulhouse3.gameObject.SetActive(false);
+            return;
+        }
+        if (NumberGears == 1)
+        {
+            Roue_Mulhouse1.gameObject.SetActive(true);
+            Roue_Mulhouse2.gameObject.SetActive(false);
+            Roue_Mulhouse3.gameObject.SetActive(false);
+            return;
+        }
+        if (NumberGears == 0)
+        {
+            Roue_Mulhouse1.gameObject.SetActive(false);
+            Roue_Mulhouse2.gameObject.SetActive(false);
+            Roue_Mulhouse3.gameObject.SetActive(false);
+            return;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Roue_Item")
+        {
             IsPickeable = true;
+            Roue = collision.gameObject;    
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Roue_Item")
+        {
             IsPickeable = false;
+            Roue = null;
+        }
     }
 
     private void PickItem ()
     {
-        Destroy(gameObject);
+        Destroy(Roue);
+        NumberGears += 1;
     }
 }
